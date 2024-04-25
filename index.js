22634d7ef77c8e96a517d431f7d6ffa5faea19ae
lobna3 <yousfi55lobna@gmail.com>
2024-04-25 13:56:34 +0200

    add

--- index.js
+++ index.js
@@ -167,7 +167,7 @@ var displayAll = function () {
         ${element.price} Euro </td><td> ${element.quantity } </td><td><img src="../images/1.jpg"></td><td><i></i></td></tr>`)
         $("#list img").attr('src', element.images[i])
         $("i").addClass("fas fa-trash")
-        
+
         /**  $("#list").append("<tr><td>" + element.id +
              "</td><td>" + element.name + "</td><td>" + element.category + "</td><td>" +
              element.price + " " + "Euro" +
@@ -193,7 +193,7 @@ productShop.removeProduct(0)
 productShop.removeProductById(1)
 productShop.updateProduct(0, 2.99)
 productShop.display(1)
-productShop.displayAll()
+//productShop.displayAll()
 
 
 //jquery
